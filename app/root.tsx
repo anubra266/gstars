2f89d48aa0e901899b6d5e2e52a0aa2c6e8af76c
Abraham <anubra266@gmail.com>
2022-01-07 19:12:32 +0100

    🔧 update chakra ui configurations to support global docuent styling

--- app/root.tsx
+++ app/root.tsx
@@ -1,12 +1,16 @@
 import {
+  ErrorBoundaryComponent,
   Links,
   LiveReload,
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
+  ScrollRestoration,
+  useCatch,
 } from "remix";
 import type { MetaFunction } from "remix";
+import { Button, ChakraProvider, Heading } from "@chakra-ui/react";
+import { ChakraDocument, ChakraStyles } from "~/chakraDocument";
 
 export const meta: MetaFunction = () => {
   return { title: "New Remix App" };
@@ -14,19 +18,81 @@ export const meta: MetaFunction = () => {
 
 export default function App() {
   return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
-      </body>
-    </html>
+    <ChakraDocument>
+      {({ emotionCache }) => (
+        <html lang="en">
+          <head>
+            <meta charSet="utf-8" />
+            <meta
+              name="viewport"
+              content="width=device-width,initial-scale=1"
+            />
+            <Meta />
+            <Links />
+            <ChakraStyles emotionCache={emotionCache} />
+          </head>
+          <body>
+            <ChakraProvider>
+              <Outlet />
+            </ChakraProvider>
+            <ScrollRestoration />
+            <Scripts />
+            {process.env.NODE_ENV === "development" && <LiveReload />}
+          </body>
+        </html>
+      )}
+    </ChakraDocument>
   );
 }
+
+export function CatchBoundary() {
+  const caught = useCatch();
+  return (
+    <ChakraDocument>
+      {({ emotionCache }) => (
+        <html>
+          <head>
+            <title>Oops!</title>
+            <Meta />
+            <Links />
+            <ChakraStyles emotionCache={emotionCache} />
+          </head>
+          <body>
+            <ChakraProvider>
+              <Heading>
+                {caught.status} - {caught.statusText}
+              </Heading>
+              <Button>Go back</Button>
+            </ChakraProvider>
+            <Scripts />
+          </body>
+        </html>
+      )}
+    </ChakraDocument>
+  );
+}
+
+export const ErrorBoundary: ErrorBoundaryComponent = ({ error }) => {
+  console.error(error);
+  return (
+    <ChakraDocument>
+      {({ emotionCache }) => (
+        <html>
+          <head>
+            <title>Oh no!</title>
+            <Meta />
+            <Links />
+            <ChakraStyles emotionCache={emotionCache} />
+          </head>
+          <body>
+            <ChakraProvider>
+              Error
+              {/* add the UI you want your users to see */}
+            </ChakraProvider>
+            <Scripts />
+          </body>
+        </html>
+      )}
+    </ChakraDocument>
+  );
+};
